459164248456ea3aae0c8c1915199f653a3bed99
Stoffy92 <38707041+Stoffy92@users.noreply.github.com>
2022-09-05 16:47:48 +1200

    basic grid

---
+++ src/Bingo.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Grid, GridItem } from '@chakra-ui/react';
+
+// Bingo component
+const Bingo = () => {
+  // reset bingo
+  const reset = () => {
+    return <button style={{ backgroundColor: 'red' }}> reset</button>;
+  };
+
+  // build UI for bingo grid
+  const generateTiles = () => {
+    let tiles = [];
+
+    for (let tile = 0; tile < 25; tile++) {
+      tiles.push(
+        <GridItem w="100%" h="10" bg="blue.500" key={tile}>
+          {tile === 12 ? 'FREE' : tile}
+        </GridItem>
+      );
+    }
+
+    return (
+      <Grid templateColumns="repeat(5, 1fr)" className="bingo__tile_container">
+        {tiles}
+      </Grid>
+    );
+  };
+
+  return (
+    <div className="bingo__container">
+      hello from bingo {reset()}
+      {generateTiles()}
+    </div>
+  );
+};
+
+export default Bingo;
